58fa1107cbd0c718cd16b175cd6216416bb35ffc
roronya <roronya628@gmail.com>
2021-09-20 13:46:22 +0900

    セリフを描画した

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
 	"image"
+	"image/color"
 	"image/draw"
 	"image/png"
+	"io"
 	"log"
 	"os"
+
+	"github.com/golang/freetype/truetype"
+)
+
+const (
+	WIDTH, HEIGHT = 1280, 720
+	FIGURE_POINT_MIN_X = 240
+	FIGURE_POINT_MIN_Y = 10
+	FIGURE_POINT_MAX_X = 1040
+	FIGURE_POINT_MAX_Y = 466
+	FONT_SIZE = 26
+	// コメントの場所は何度か試して適当に決めた
+	R_COMMENT_POINT_X = 250
+	R_COMMENT_POINT_Y = 530
+	R_COMMENT2_POINT_Y = R_COMMENT_POINT_Y + FONT_SIZE + 5 // 5は行間のサイズ
+	L_COMMENT_POINT_X = 250
+	L_COMMENT_POINT_Y = 645
+	L_COMMENT2_POINT_Y = L_COMMENT_POINT_Y + FONT_SIZE + 5
 )
 
 func main () {
-	const (
-		width, height = 1280, 720
-	)
 	f := "img/test.png"
 
 	file, err := os.Open("assets/lr.png")
@@ -38,13 +57,13 @@ func main () {
 	dst := image.NewRGBA(
 		image.Rectangle{
 		Min: image.Point{},
-		Max: image.Point{X: width, Y: height},
+		Max: image.Point{X: WIDTH, Y: HEIGHT},
 	})
 	// templateの画像を上に乗せる
 	draw.Draw(
 		dst,
 		// 第2引数は下地になる画像のどの範囲に上に乗せるかを指定する。templateで完全に覆うので(0,0)と(1280,720)を指定する
-		image.Rectangle{Min: image.Point{}, Max: image.Point{X: width, Y: height}},
+		image.Rectangle{Min: image.Point{}, Max: image.Point{X: WIDTH, Y: HEIGHT}},
 		template,
 		//　第4引数は上に乗せる画像を指定したpointから切り取る。切り取る必要はないので(0, 0)を指定する
 		image.Point{},
@@ -55,10 +74,44 @@ func main () {
 		dst,
 		// 解説対象の画像は800*450なので左からは(1280-800)/2=240pxの場所に置く。画面上部のマージンはなんとなく10pxくらい
 		// 右下は左隅から右に240+800=1040pxで、縦は16+450=466pxを指定する
-		image.Rectangle{Min: image.Point{X: 240, Y: 10}, Max: image.Point{X: 1040, Y: 466}},
+		image.Rectangle{
+			Min: image.Point{X: FIGURE_POINT_MIN_X, Y: FIGURE_POINT_MIN_Y},
+			Max: image.Point{X: FIGURE_POINT_MAX_X, Y: FIGURE_POINT_MAX_Y}},
 		figure,
 		image.Point{}, // 切り取らないから(0,0)を指定する
 		draw.Src,
 	)
+
+	ttf, err := os.Open("assets/ipaexg.ttf")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ftBin, err := io.ReadAll(ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ft, err := truetype.Parse(ftBin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	drawComment("ほげほげほげほげ", ft, R_COMMENT_POINT_X, R_COMMENT_POINT_Y, dst)
+	drawComment("ほげほげほげほげ", ft, R_COMMENT_POINT_X, R_COMMENT2_POINT_Y, dst)
+	drawComment("ほげほげほげほげ", ft, L_COMMENT_POINT_X, L_COMMENT_POINT_Y, dst)
+	drawComment("ほげほげほげほげ", ft, R_COMMENT_POINT_X, L_COMMENT2_POINT_Y, dst)
+
 	png.Encode(os.Stdout, dst)
+}
+
+func drawComment(text string, ft *truetype.Font, x int, y int, dst *image.RGBA) {
+	opt := truetype.Options{Size: FONT_SIZE}
+	face := truetype.NewFace(ft, &opt)
+	dot := fixed.Point26_6{X: fixed.Int26_6(x*64), Y: fixed.Int26_6(y * 64)}
+	d := &font.Drawer{
+		Dst: dst,
+		Src: image.NewUniform(color.Black),
+		Face: face,
+		Dot: dot,
+	}
+	d.DrawString(text)
 }
